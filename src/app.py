adf2a235cd323f4093583d74fc5dbcf53da004f1
Indika Wijayasinghe <i.b.wijayasinghe@ieee.org>
2018-08-10 15:57:16 -0400

    Prometheus client for Python added

--- src/app.py
+++ src/app.py
@@ -1,3 +1,19 @@
-# Project RS main file
+from prometheus_client import start_http_server, Summary
+import random
+import time
 
-print "Hello world!\n"
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+# Decorate function with metric.
+@REQUEST_TIME.time()
+def process_request(t):
+	"""A dummy function that takes some time."""
+	time.sleep(t)
+
+if __name__ == '__main__':
+	# Start up the server to expose the metrics.
+	start_http_server(8000)
+	# Generate some requests.
+	while True:
+		process_request(random.random())
