7666b4c277fdbb1f5b5e0aa4bb043f302c6834f1
Indika Wijayasinghe <i.b.wijayasinghe@ieee.org>
2018-08-11 01:11:18 -0400

    Working

--- app/src/main_app.py
+++ app/src/main_app.py
@@ -1,16 +1,19 @@
 from prometheus_client import start_http_server, Gauge
-import time
+import time, httplib, json
 
-# Create a metric to track time spent and requests made.
-tv = Gauge('counter_value', 'Random')
+# Connection to the CoinDesk Bitcoin Price Index API
+conn = httplib.HTTPSConnection("api.coindesk.com")
 
-my_counter = 0
+# Create a metric to track bitcoin price.
+tv = Gauge('bitcoin_value', 'Bitcoin Price Index')
 
 if __name__ == '__main__':
 	# Start up the server to expose the metrics.
 	start_http_server(8000)
-	# Generate some requests.
+	# Generate requests each minute.
 	while True:
-		time.sleep(1)
-		tv.set(my_counter)
-		my_counter = my_counter + 1
+		conn.request("GET", "/v1/bpi/currentprice.json")
+		r = conn.getresponse()
+		r_dict = json.loads(r.read())
+		tv.set(r_dict["bpi"]["USD"]["rate_float"])
+		time.sleep(60)
