c90cc956fbab1b700f13b91a555a573021863742
Indika Wijayasinghe <i.b.wijayasinghe@ieee.org>
2018-08-11 01:21:19 -0400

    Datasource changed to Bitstamp

--- app/src/main_app.py
+++ app/src/main_app.py
@@ -1,8 +1,8 @@
 from prometheus_client import start_http_server, Gauge
 import time, httplib, json
 
-# Connection to the CoinDesk Bitcoin Price Index API
-conn = httplib.HTTPSConnection("api.coindesk.com")
+# Connection to the Bitstamp Bitcoin Price Index API
+conn = httplib.HTTPSConnection("www.bitstamp.net")
 
 # Create a metric to track bitcoin price.
 tv = Gauge('bitcoin_value', 'Bitcoin Price Index')
@@ -12,8 +12,8 @@ if __name__ == '__main__':
 	start_http_server(8000)
 	# Generate requests each minute.
 	while True:
-		conn.request("GET", "/v1/bpi/currentprice.json")
+		conn.request("GET", "/api/ticker/")
 		r = conn.getresponse()
 		r_dict = json.loads(r.read())
-		tv.set(r_dict["bpi"]["USD"]["rate_float"])
-		time.sleep(60)
+		tv.set(r_dict["last"])
+		time.sleep(15)
