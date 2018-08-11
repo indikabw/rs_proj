f4f339d3c076d624aa1bf560e9bd171d0f1a5454
Indika Wijayasinghe <i.b.wijayasinghe@ieee.org>
2018-08-11 01:28:44 -0400

    main_app.py commented

--- app/src/main_app.py
+++ app/src/main_app.py
@@ -1,19 +1,26 @@
+#	Author: Indika Wijayasinghe
+#	Email: i.b.wijayasinghe@ieee.org
+#
+#	Description: Service fetches bitcoin price from bitstamp
+#							 every 15s, and exposes the metric to prometheus
+#							 to be stored.
+
 from prometheus_client import start_http_server, Gauge
 import time, httplib, json
 
-# Connection to the Bitstamp Bitcoin Price Index API
+# Connection to Bitstamp API.
 conn = httplib.HTTPSConnection("www.bitstamp.net")
 
 # Create a metric to track bitcoin price.
-tv = Gauge('bitcoin_value', 'Bitcoin Price Index')
+bv = Gauge('bitcoin_value', 'Bitcoin Price Index')
 
 if __name__ == '__main__':
 	# Start up the server to expose the metrics.
 	start_http_server(8000)
-	# Generate requests each minute.
+	# Generate requests 15 seconds.
 	while True:
 		conn.request("GET", "/api/ticker/")
 		r = conn.getresponse()
 		r_dict = json.loads(r.read())
-		tv.set(r_dict["last"])
+		bv.set(r_dict["last"])
 		time.sleep(15)
